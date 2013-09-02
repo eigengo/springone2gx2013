319c79930ff96996c97aaea38fb23abd20a330e3
Jan Machacek <jan.machacek@gmail.com>
2013-09-02 15:49:46 +0100

    DispatcherServlet

--- jvm/src/main/scala/org/eigengo/sogx/config/DispatcherServletInitializer.scala
+++ jvm/src/main/scala/org/eigengo/sogx/config/DispatcherServletInitializer.scala
@@ -1,2 +1,24 @@
 package org.eigengo.sogx.config
 
+import javax.servlet.ServletRegistration
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer
+
+class DispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+  protected def getRootConfigClasses: Array[Class[_]] = {
+    Array[Class[_]](classOf[Webapp])
+  }
+
+  protected def getServletConfigClasses: Array[Class[_]] = {
+    Array[Class[_]](classOf[Webapp])
+  }
+
+  protected def getServletMappings: Array[String] = {
+    Array[String]("/")
+  }
+
+  protected override def customizeRegistration(registration: ServletRegistration.Dynamic) {
+    registration.setInitParameter("dispatchOptionsRequest", "true")
+  }
+
+}
--- jvm/src/main/scala/org/eigengo/sogx/config/Webapp.scala
+++ jvm/src/main/scala/org/eigengo/sogx/config/Webapp.scala
@@ -5,4 +5,29 @@ import org.springframework.web.servlet.config.annotation.{DefaultServletHandlerC
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import org.eigengo.sogx._
 import org.springframework.integration.annotation.{Payload, Header}
+import org.eigengo.sogx.core.RecogServiceActivator
 
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages=Array("org.eigengo.sogx"))
+class Webapp extends WebMvcConfigurerAdapter with WebConfig with CoreConfig {
+
+  @Bean def asyncExecutor() = {
+    val executor = new ThreadPoolTaskExecutor
+    executor.setCorePoolSize(4)
+    executor.setCorePoolSize(8)
+    executor.setThreadNamePrefix("MessageChannel-")
+    executor
+  }
+
+  @Bean def recogServiceActivator() = new RecogServiceActivator {
+    def onCoinResponse(@Header correlationId: CorrelationId, @Payload coins: CoinResponse): Unit =
+      recogSessions().onCoinResponse(correlationId, coins)
+  }
+
+  // Allow serving HTML files through the default Servlet
+  override def configureDefaultServletHandling(configurer: DefaultServletHandlerConfigurer) = {
+    configurer.enable()
+  }
+
+}
