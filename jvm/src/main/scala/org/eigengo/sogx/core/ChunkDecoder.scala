a87732eba651be417319aa5e2b5b325a300f6bdc
Jan Machacek <jan.machacek@gmail.com>
2013-09-02 14:58:17 +0100

    ChunkDecoder

--- jvm/src/main/scala/org/eigengo/sogx/core/ChunkDecoder.scala
+++ jvm/src/main/scala/org/eigengo/sogx/core/ChunkDecoder.scala
@@ -6,3 +6,26 @@ import org.eigengo.sogx.ContentTypes._
 import org.eigengo.sogx._
 import java.util
 
+class ChunkDecoder(mjpegDecoder: MJPEGDecoder) {
+
+  /**
+   * Take the chunk arriving on a particular correlationId, examine its content type, and attempt to decode as many
+   * still frames as possible; now that we have all previous chunks and the new one just arriving.
+   *
+   * @param correlationId the correlation id
+   * @param contentType the content type
+   * @param chunk the new chunk
+   * @return collection of individual frames (represented as JPEG data)
+   */
+  def decodeFrame(@Header correlationId: CorrelationId, @Header("content-type") contentType: String,
+                  @Payload chunk: ChunkData): util.Collection[ImageData] = contentType match {
+    case `video/mjpeg` => decodeMJPEGFrames(correlationId, chunk)
+    case `image/*`     => decodeSingleImage(correlationId, chunk)
+  }
+
+  private def decodeSingleImage(correlationId: CorrelationId, chunk: ChunkData): util.Collection[ImageData] = Collections.singletonList(chunk)
+
+  private def decodeMJPEGFrames(correlationId: CorrelationId, chunk: ChunkData): util.Collection[ImageData] = mjpegDecoder.decodeFrames(correlationId, chunk)
+
+
+}
--- jvm/src/main/scala/org/eigengo/sogx/core/RecogService.scala
+++ jvm/src/main/scala/org/eigengo/sogx/core/RecogService.scala
@@ -4,14 +4,33 @@ import org.eigengo.sogx._
 import org.springframework.integration.MessageChannel
 import org.springframework.integration.support.MessageBuilder
 
-class RecogService {
+class RecogService(recogChannel: MessageChannel) {
 
-  def mjpegChunk(id: CorrelationId)(data: ChunkData): Unit = {
+  private def sendWithContentType(contentType: String, correlationId: CorrelationId, chunk: ChunkData): Unit = {
+    val message = MessageBuilder.
+      withPayload(chunk).
+      setCorrelationId(correlationId).
+      setHeader("content-type", contentType).
+      build()
 
+    recogChannel.send(message)
   }
 
-  def imageChunk(id: CorrelationId)(data: ChunkData): Unit = {
+  /**
+   * Submits a chunk of data that represents full image (JPEG, PNG, ...)
+   *
+   * @param correlationId the correlation id
+   * @param chunk the chunk representing the entire image payload
+   */
+  def imageChunk(correlationId: CorrelationId)(chunk: ChunkData) = sendWithContentType(ContentTypes.`image/*`, correlationId, chunk)
 
-  }
+  /**
+   * Submits a chunk of data that represents portion of MJPEG video stream
+   *
+   * @param correlationId the correlation id
+   * @param chunk the chunk of data of the MJPEG stream
+   */
+  def mjpegChunk(correlationId: CorrelationId)(chunk: ChunkData) = sendWithContentType(ContentTypes.`video/mjpeg`, correlationId, chunk)
+
 
 }
